edbf664c901e97cf522a33af703a057426e3ad91
fredlc <fredlefevre@outlook.com>
2020-07-26 12:24:23 -0400

    Header and footer

---
+++ src/components/Header-Footer/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <div class="container header_container">
+      <div class="logo">
+        <a href="/">Gamespot</a>
+      </div>
+      <div class="login">
+        <a href="/signin">
+          <img
+            :src="require('../../assets/images/login.png')"
+            alt="login icon"
+          />
+        </a>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
